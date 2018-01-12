614f196572edbf6f2edb12111262c6c1276731a0
JeongSeongKyun <sunken37@gmail.com>
2018-01-12 14:26:22 +0900

    [update] Fix for virtualenv installs

--- rest_framework_cache/__init__.py
+++ rest_framework_cache/__init__.py
@@ -1,9 +1,5 @@
-import pkg_resources
-
-
 __author__ = 'Ervilis Souza'
 __email__ = 'ervilisviana@gmail.com'
-__version__ = '0.1'
+__version__ = '0.1.1'
 
-
-pkg_resources.declare_namespace(__name__)
+default_app_config = 'rest_framework_cache.apps.RestFrameworkCacheConfig'
---
+++ rest_framework_cache/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class RestFrameworkCacheConfig(AppConfig):
+    name = 'rest_framework_cache'
+    verbose_name = "REST Framework Cache"
